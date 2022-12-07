1891ac08e3f3584de15197919494e499b8430d76
duro06 <andi.meka.025@gmail.com>
2022-12-07 13:51:50 +0700

    revisi master min max depo dan pengguna

---
+++ src/pages/simrs/sigarang/master/minmaxstok/FrontPage.vue
@@ -0,0 +1,5 @@
+<template>
+  depan
+</template>
+<script setup>
+</script>
--- src/pages/simrs/sigarang/master/minmaxstok/IndexPage.vue
+++ src/pages/simrs/sigarang/master/minmaxstok/IndexPage.vue
@@ -14,6 +14,6 @@
 import { defineAsyncComponent } from 'vue'
 
 const AsyncComp = defineAsyncComponent(() =>
-  import('./MinMaxStokPage.vue')
+  import('./FrontPage.vue')
 )
 </script>
