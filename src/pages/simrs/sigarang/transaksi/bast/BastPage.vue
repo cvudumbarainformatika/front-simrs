86460e2d38fa25bb570e95cd78eca02aabaec602
duro06 <andi.meka.025@gmail.com>
2023-04-17 14:26:16 +0700

    udah

--- src/pages/simrs/sigarang/transaksi/bast/BastPage.vue
+++ src/pages/simrs/sigarang/transaksi/bast/BastPage.vue
@@ -26,20 +26,20 @@
             </div>
             <div class="row q-col-gutter-sm items-center q-mb-sm">
               <div class="col-4">
-                Nomor Pemesanan
+                Nomor kontrak
               </div>
               <div class="col-8">
                 <app-autocomplete-new
-                  v-model="store.form.nomor"
-                  label="pilih Pemesanan"
-                  autocomplete="nomor"
-                  option-label="nomor"
-                  option-value="nomor"
+                  v-model="store.form.kontrak"
+                  label="pilih Kontrak"
+                  autocomplete="kontrak"
+                  option-label="kontrak"
+                  option-value="kontrak"
                   outlined
                   valid
                   :loading="store.loading"
-                  :source="store.pemesanans"
-                  @on-select="store.pemesananSelected"
+                  :source="store.kontraks"
+                  @on-select="store.kontrakSelected"
                 />
               </div>
             </div>
@@ -94,12 +94,16 @@
           />
         </div>
       </q-card-section>
+      <!--
       <q-card-section>
-        <div class="f-14 text-weight-bold q-mb-sm">
+          <div class="f-14 text-weight-bold q-mb-sm">
+          Daftar Pemesanan
+        </div> -->
+
+      <!-- <div class="f-14 text-weight-bold q-mb-sm">
           Daftar Penerimaan
         </div>
         <div v-if="store.penerimaans.length">
-          <!-- {{ store.penerimaans }} -->
           <q-list
             bordered
             separator
@@ -134,7 +138,6 @@
               v-ripple
               clickable
             >
-              <!-- @click="itemClicked(item,i)" -->
               <q-item-section>
                 <q-item-label>
                   <div class="row fit no-wrap justify-start items-center q-mb-sm">
@@ -238,7 +241,6 @@
                         {{ formatRpDouble(det.harga,2) }}
                       </div>
                       <div class="deta">
-                        <!-- {{ formatRpDouble(det.harga_kontrak,2) }} -->
                         <app-input
                           v-model="det.harga_kontrak"
                           outlined
@@ -250,7 +252,6 @@
                         />
                       </div>
                       <div class="deta">
-                        <!-- {{ det.diskon }} -->
                         <app-input
                           v-model="det.diskon"
                           outlined
@@ -261,7 +262,6 @@
                         />
                       </div>
                       <div class="deta">
-                        <!-- {{ det.ppn }} -->
                         <app-input
                           v-model="det.ppn"
                           outlined
@@ -273,12 +273,7 @@
                       </div>
                       <div class="deta">
                         {{ formatRpDouble(det.harga_jadi,2) }}
-                        <!-- <app-input
-                          v-model="det.harga_jadi"
-                          outlined
-                          label="harga jadi"
-                          dense
-                        /> -->
+
                       </div>
                     </div>
                     <div class="row fit no-wrap items-center q-mb-sm">
@@ -308,7 +303,8 @@
           <app-no-data />
         </div>
       </q-card-section>
-      <q-card-actions align="right">
+        -->
+      <!-- <q-card-actions align="right">
         <div
           v-if="store.tampilPenerimaans.length"
           class="row justify-end"
@@ -320,7 +316,240 @@
             @click="simpanBast"
           />
         </div>
-      </q-card-actions>
+      </q-card-actions> -->
+    </q-card>
+    <q-card v-if="store.pemesanans.length">
+      <q-card-section>
+        <div
+          class="row fit no-wrap items-center justify-start q-mb-sm q-col-gutter-sm text-weight-bold bb"
+        >
+          <div
+            class="col-1"
+          />
+          <div class="col-5">
+            Nomor Pemesanan
+          </div>
+          <div class="col-3">
+            Tanggal
+          </div>
+          <div class="col-3">
+            Total Pesanan
+          </div>
+        </div>
+        <div
+          v-for="(pesan, n) in store.pemesanans"
+          ref="refPemesanans"
+          :key="n"
+        >
+          <div
+            class="row fit no-wrap items-center justify-start q-mb-sm q-col-gutter-sm"
+            :class="pesan.checked?'':'bb'"
+          >
+            <div
+              class="col-1 cursor-pointer"
+              @click="pesanClicked(pesan,n)"
+            >
+              <q-checkbox
+                v-model="pesan.checked"
+                size="xs"
+                dense
+                @click="pesanClicked(pesan,n)"
+              />
+            </div>
+            <div class="col-5">
+              {{ pesan.nomor }}
+            </div>
+            <div class="col-3">
+              {{ dateFullFormat(pesan.tanggal) }}
+            </div>
+            <div class="col-3">
+              {{ formatRpDouble(pesan.total) }}
+            </div>
+          </div>
+          <div v-if="pesan.checked && pesan.penerimaan.length">
+            <div class="row fit no-wrap justify-start items-center text-weight-bolder">
+              <div class="anak">
+                Nomor Faktur
+              </div>
+              <div class="anak">
+                Tanggal Faktur
+              </div>
+              <div class="anak">
+                Nilai faktur
+              </div>
+              <div class="anak">
+                Nomor Pemesanan
+              </div>
+              <div class="anak">
+                Nomor Penerimaan
+              </div>
+            </div>
+            <div
+              v-for="(item,i) in pesan.penerimaan"
+              ref="refPenerimaans"
+              :key="i"
+            >
+              <div
+                class="row fit no-wrap items-center justify-start q-mb-sm q-col-gutter-sm"
+                :class="item.checked?'':'bb'"
+              >
+                <div
+                  class="anak"
+                  @click="itemClicked(item,i)"
+                >
+                  <div class="row">
+                    <div
+                      class="col-1"
+                      @click="itemClicked(item,i)"
+                    >
+                      <q-checkbox
+                        v-model="item.checked"
+                        size="xs"
+                        dense
+                        @click="itemClicked(item,i)"
+                        @update:model-value="checkBox"
+                      />
+                    </div>
+                    <div class="col-11">
+                      {{ item.faktur?item.faktur:'-' }}
+                    </div>
+                  </div>
+                </div>
+                <div class="anak">
+                  {{ item.tanggal_faktur?dateFullFormat( item.tanggal_faktur):'-' }}
+                </div>
+                <div class="anak">
+                  {{ item.nilai_tagihan?formatRpDouble(item.nilai_tagihan,2):'-' }}
+                </div>
+                <div class="anak">
+                  {{ item.nomor }}
+                </div>
+                <div class="anak">
+                  {{ item.no_penerimaan }}
+                </div>
+              </div>
+              <div v-if="item.checked">
+                <div class="row q-mt-sm bb">
+                  <div class="q-mb-md">
+                    Daftar Barang Pesanan <span class="text-weight-bold">{{ item.nomor }}</span>
+                  </div>
+                  <div class="row fit no-wrap items-center q-mb-sm">
+                    <div class="deta text-weight-bold">
+                      <div class="row">
+                        <div class="col-2">
+                          No
+                        </div>
+                        <div class="col-10">
+                          Nama Barang
+                        </div>
+                      </div>
+                    </div>
+                    <div class="deta text-weight-bold">
+                      Satuan
+                    </div>
+                    <div class="deta text-weight-bold">
+                      Kuantitas
+                    </div>
+                    <div class="deta text-weight-bold">
+                      Harga Pemesanan
+                    </div>
+                    <div class="deta text-weight-bold">
+                      Harga Kontrak
+                    </div>
+                    <div class="deta text-weight-bold">
+                      Diskon (Rp)
+                    </div>
+                    <div class="deta text-weight-bold">
+                      PPN (%)
+                    </div>
+                    <div class="deta text-weight-bold">
+                      Jumlah
+                    </div>
+                  </div>
+                  <div
+                    v-for="(det, j) in item.details"
+                    ref="refDetails"
+                    :key="j"
+                    class="row fit no-wrap items-center q-mb-xs"
+                  >
+                    <div class="deta">
+                      <div class="row">
+                        <div class="col-2">
+                          {{ j+1 }}
+                        </div>
+                        <div class="col-10">
+                          {{ det.nama_barang }}
+                        </div>
+                      </div>
+                    </div>
+                    <div class="deta">
+                      {{ det.satuan_besar }}
+                    </div>
+                    <div class="deta">
+                      {{ det.qty }}
+                    </div>
+                    <div class="deta">
+                      {{ formatRpDouble(det.harga,2) }}
+                    </div>
+                    <div class="deta">
+                      <app-input
+                        ref="refHargaKontrak"
+                        v-model="det.harga_kontrak"
+                        outlined
+                        label="harga kontrak"
+                        dense
+                        type="number"
+                        :rules="[val=> val > 0 ||'Harus lebih besar dari 0']"
+                        @focus="assign(n,i,j)"
+                        @update:model-value="updateHarga"
+                      />
+                    </div>
+                    <div class="deta">
+                      <app-input
+                        v-model="det.diskon"
+                        outlined
+                        label="diskon"
+                        dense
+                        type="number"
+                        @focus="assign(n,i,j)"
+                        @update:model-value="updateHarga"
+                      />
+                    </div>
+                    <div class="deta">
+                      <app-input
+                        v-model="det.ppn"
+                        outlined
+                        label="PPN"
+                        dense
+                        type="number"
+                        @focus="assign(n,i,j)"
+                        @update:model-value="updateHarga"
+                      />
+                    </div>
+                    <div class="deta">
+                      {{ formatRpDouble(det.harga_jadi,2) }}
+                    </div>
+                  </div>
+                  <div class="row fit no-wrap items-center q-mb-sm">
+                    <div class="deta text-weight-bold" />
+                    <div class="deta text-weight-bold" />
+                    <div class="deta text-weight-bold" />
+                    <div class="deta text-weight-bold" />
+                    <div class="deta text-weight-bold" />
+                    <div class="deta text-weight-bold" />
+                    <div class="deta text-weight-bold">
+                      Total
+                    </div>
+                    <div class="deta text-weight-bold">
+                      {{ formatRpDouble(item.nilai_tagihan,2) }}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </q-card-section>
     </q-card>
   </div>
 </template>
@@ -345,16 +574,26 @@ function setTanggal(val) {
   store.setForm('tanggal_bast', date.formatDate(val, 'YYYY-MM-DD'))
 }
 
+function pesanClicked(val, i) {
+  console.log('pesanan', val)
+}
+let indPem = 0
 let indPene = 0
 let indDet = 0
-function assign(i, j) {
+function assign(n, i, j) {
+  indPem = n
   indPene = i
   indDet = j
 }
 function updateHarga(val) {
   // console.log(val)
-  store.penerimaans[indPene].details[indDet].harga_jadi = parseFloat(store.penerimaans[indPene].details[indDet].harga_kontrak) - parseFloat(store.penerimaans[indPene].details[indDet].diskon) + (parseFloat(store.penerimaans[indPene].details[indDet].harga_kontrak) * parseFloat(store.penerimaans[indPene].details[indDet].ppn) / 100)
-  store.penerimaans[indPene].nilai_tagihan = store.penerimaans[indPene].details.map(x => x.harga_jadi).reduce((a, b) => a + b, 0)
+  store.pemesanans[indPem].penerimaan[indPene].details[indDet].harga_jadi =
+  (store.pemesanans[indPem].penerimaan[indPene].details[indDet].qty *
+  parseFloat(store.pemesanans[indPem].penerimaan[indPene].details[indDet].harga_kontrak)) -
+  parseFloat(store.pemesanans[indPem].penerimaan[indPene].details[indDet].diskon) +
+  ((parseFloat(store.pemesanans[indPem].penerimaan[indPene].details[indDet].harga_kontrak * store.pemesanans[indPem].penerimaan[indPene].details[indDet].qty) *
+  parseFloat(store.pemesanans[indPem].penerimaan[indPene].details[indDet].ppn) / 100))
+  store.pemesanans[indPem].penerimaan[indPene].nilai_tagihan = store.pemesanans[indPem].penerimaan[indPene].details.map(x => x.harga_jadi).reduce((a, b) => a + b, 0)
 }
 
 function checkBox(val) {
@@ -362,7 +601,7 @@ function checkBox(val) {
 }
 function itemClicked(val, i) {
   console.log('item clicked', val)
-  val.checked = !val.checked
+  // val.checked = !val.checked
   const ind = findWithAttr(store.tampilPenerimaans, 'id', val.id)
   if (val.checked === true) {
     if (ind < 0) {
@@ -382,12 +621,31 @@ function itemClicked(val, i) {
 }
 const refTaBast = ref(null)
 const refNoBast = ref(null)
+const refHargaKontrak = ref(null)
+const refPemesanans = ref(null)
+const refPenerimaans = ref(null)
+const refDetails = ref(null)
 function simpanBast() {
-  // console.log('no bast', refNoBast.value.$refs.refInput.resetValidation())
+  refHargaKontrak.value.forEach(kuylah => {
+    console.log('harga Kontrak', kuylah.refInput.validate())
+  })
+  // console.log('harga Kontrak', refHargaKontrak.value[0].refInput)
   // store.form.details = store.penerimaans.filter(x => x.checked)
-  // console.log('form', store.form)
   if (refTaBast.value.$refs.refInputDate.validate()) {
-    store.form.penerimaans = store.penerimaans.filter(x => x.checked)
+    const temp = store.pemesanans
+    store.form.penerimaans = temp.map(psn => {
+      const anu = psn
+      psn.penerimaan = psn.penerimaan.filter(trm => trm.checked)
+      if (anu.penerimaan.length) return anu.penerimaan
+      else return false
+    }).filter(x => x !== false)
+    // store.form.pemesanans = temp.map(psn => {
+    //   const anu = psn
+    //   psn.penerimaan = psn.penerimaan.filter(trm => trm.checked)
+    //   return anu
+    // }).filter(x => x.checked)
+    console.log('form', store.form)
+    console.log('pemesanan', store.pemesanans)
     store.simpanBast().then(() => {
       refNoBast.value.$refs.refInput.resetValidation()
       refTaBast.value.$refs.refInputDate.resetValidation()
@@ -412,5 +670,11 @@ function simpanBast() {
   width:calc(100vw/9);
   margin-right: .5em;
   overflow-wrap: anywhere;
+}
+.wrap-it{
+  overflow-wrap: anywhere;
+}
+.bb{
+  border-bottom: 1px solid black;
 }
 </style>
