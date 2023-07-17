41461b306d72c6c856cfb811bdfcefe87b106549
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-07-17 09:24:06 +0700

    cxvc

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -10,8 +10,8 @@ import { notifErr } from 'src/modules/utils'
 // const SERV = 'http://192.168.150.103:3507'
 // const SERV = 'https://xenter.my.id'
 // const SERV = 'http://192.168.150.103:3501' // gawe laborat
-// const SERV = 'http://192.168.101.80/api.laborat/public'
-const SERV = 'http://localhost/api.laborat/public'
+const SERV = 'http://192.168.101.80/api.laborat/public'
+// const SERV = 'http://localhost/api.laborat/public'
 // const SERV = 'https://rsudmochsaleh.my.id'
 const SERVER = SERV + '/api'
 
