62d303a411e76c73abed435a7af0a221e08c9442
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-07-26 15:14:06 +0700

    sdasd

---
+++ src/pages/simrs/PrintAntrian.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="printMe">
+    <div>
+      Cobak Print Antrian
+    </div>
+
+    <q-btn
+      ref="printBtn"
+      label="print coba"
+      @click="print"
+    />
+  </div>
+</template>
+
+<script setup>
+// import VueHtmlToPaper from 'src/boot/VueHtmlToPaper'
+import { onMounted, ref } from 'vue'
+
+const printBtn = ref()
+
+onMounted(() => {
+  console.log('mounted print')
+})
+
+function print() {
+  this.$htmlToPaper('printMe')
+}
+</script>
