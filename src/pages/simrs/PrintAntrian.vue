ea9b22a0325704f03660acb0d63b595664688b8f
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-07-26 23:02:29 +0700

    ffff

--- src/pages/simrs/PrintAntrian.vue
+++ src/pages/simrs/PrintAntrian.vue
@@ -6,8 +6,8 @@
 
     <q-btn
       ref="printBtn"
+      v-print="printObj"
       label="print coba"
-      @click="print"
     />
   </div>
 </template>
@@ -18,12 +18,31 @@ import { onMounted, ref } from 'vue'
 
 const printBtn = ref()
 
-onMounted(() => {
-  console.log('mounted print')
+const printObj = ref({
+  id: 'printMe',
+  popTitle: 'Laporan Rekap',
+  // extraCss: 'https://cdn.bootcdn.net/ajax/libs/animate.css/4.1.1/animate.compat.css, https://cdn.bootcdn.net/ajax/libs/hover.css/2.3.1/css/hover-min.css',
+  // extraHead: '<meta http-equiv="Content-Language"content="zh-cn"/>',
+  beforeOpenCallback(vue) {
+    // printed.value = true
+    console.log('wait...')
+  },
+  openCallback (vue) {
+    console.log('opened')
+  },
+  closeCallback (vue) {
+    // printed.value = false
+    console.log('closePrint')
+  }
 })
 
-async function print () {
-  // Pass the element id here
-  await this.$htmlToPaper('printMe')
-}
+onMounted(() => {
+  console.log('mounted print', printBtn.value.$.appContext.app.directive)
+  printBtn.value.print(printObj)
+})
+
+// async function print () {
+//   // Pass the element id here
+//   await this.$htmlToPaper('printMe')
+// }
 </script>
