b3a4ccf62d796b2869112dcadf9d9a0778ecfb5a
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-08-09 14:55:14 +0700

    asdas

---
+++ src/pages/simrs/poli/pengunjung/comp/comptindakan/TindakanContent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <AnamnesisPage v-if="tab==='anamnesis'" />
+    <PemeriksaanPage v-else-if="tab==='pemeriksaan'" />
+  </div>
+</template>
+<script setup>
+import AnamnesisPage from './anamnesis/AnamnesisPage.vue'
+import PemeriksaanPage from './pemeriksaan/PemeriksaanPage.vue'
+defineProps({
+  tab: {
+    type: String,
+    default: 'kosong'
+  }
+})
+</script>
