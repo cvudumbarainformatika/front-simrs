ef6e273351c9c458bc37547466e864a102efbe80
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-08-14 01:38:00 +0700

    dfg

--- src/pages/simrs/poli/tindakan/comptindakan/pagemenu/AnamnesisPage.vue
+++ src/pages/simrs/poli/tindakan/comptindakan/pagemenu/AnamnesisPage.vue
@@ -422,34 +422,6 @@ const rows = ref([
 ])
 </script>
 
-<!-- <style lang="sass">
-.my-sticky-header-table
-  /* height or max-height is important */
-  height: 310px
-
-  .q-table__top,
-  .q-table__bottom,
-  thead tr:first-child th
-    /* bg color is important for th; just specify one */
-    background-color: #00b4ff
-
-  thead tr th
-    position: sticky
-    z-index: 1
-  thead tr:first-child th
-    top: 0
-
-  /* this is when the loading indicator appears */
-  &.q-table--loading thead tr:last-child th
-    /* height of all previous header rows */
-    top: 48px
-
-  /* prevent scrolling behind sticky top row on focus */
-  tbody
-    /* height of all previous header rows */
-    scroll-margin-top: 48px
-</style> -->
-
 <style lang="scss">
 .my-sticky-header-table{
   height: 290px;
