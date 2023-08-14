b033905893f8ebc7244064c6785b728358a256fd
duro06 <andi.meka.025@gmail.com>
2023-08-14 23:17:11 +0700

    sudah dulu

--- src/pages/simrs/farmasi/pemesanan/rencana/RencanaPage.vue
+++ src/pages/simrs/farmasi/pemesanan/rencana/RencanaPage.vue
@@ -44,60 +44,121 @@
   </div>
   <!-- perencanaan -->
   <div class="q-mt-lg q-pt-lg">
-    <app-table
-      title="obat"
-      :columns="table.columns"
-      :column-hide="table.columnHide"
-      :items="table.items"
-      :meta="table.meta"
-      :per-page="table.params.per_page"
-      :order-by="table.params.order_by"
-      :sort="table.params.sort"
-      :loading="table.loading"
-      :to-search="table.params.q"
-      :ada-edit="false"
-      :ada-paginasi="false"
-      @goto="table.setPage"
-      @set-row="table.setPerPage"
-      @refresh="table.refreshTable"
-      @find="table.setSearch"
-      @set-order="table.setOder"
-      @new-data="store.newData"
-      @delete="table.deletesData"
-    >
-      <!-- @edit-data="store.editData" -->
-      <!--
+    <q-card>
+      <q-card-section>
+        <app-table
+          title="obat"
+          :columns="table.columns"
+          :column-hide="table.columnHide"
+          :items="table.items"
+          :meta="table.meta"
+          :per-page="table.params.per_page"
+          :order-by="table.params.order_by"
+          :sort="table.params.sort"
+          :loading="table.loading"
+          :to-search="table.params.q"
+          :default-btn="false"
+          :ada-paginasi="false"
+          @goto="table.setPage"
+          @set-row="table.setPerPage"
+          @refresh="table.refreshTable"
+          @find="table.setSearch"
+          @set-order="table.setOder"
+          @new-data="store.newData"
+          @delete="table.deletesData"
+        >
+          <!-- @edit-data="store.editData" -->
+          <!--
             row-image="image"
             @delete-ids="table.deletesData"
             -->
-      <template #col-bentuksediaan>
-        <div>Bentuk Sediaan</div>
-      </template>
-      <template #cell-stok_gudang="{row}">
-        <div v-if="row.stokrealgudang.length">
-          {{ row.stokrealgudang[0].jumlah }}
-        </div>
-        <div v-else>
-          -
-        </div>
-      </template>
-      <template #cell-stok_rs="{row}">
-        <div v-if="row.stokrealallrs.length">
-          {{ row.stokrealallrs[0].jumlah }}
-        </div>
-        <div v-else>
-          -
-        </div>
-      </template>
-      <template #cell-stok_max_rs="{row}">
-        <div v-if="row.stokmaxrs.length">
-          {{ row.stokmaxrs[0].jumlah }}
-        </div>
-        <div v-else>
-          -
-        </div>
-      </template>
-    </app-table>
+          <template #col-kd_obat>
+            <div>Kode Obat</div>
+          </template>
+          <template #col-nama_obat>
+            <div>Nama Obat</div>
+          </template>
+          <template #col-stok>
+            <div>Stok</div>
+          </template>
+          <template #col-pabrikan>
+            <div>Penyedia</div>
+          </template>
+          <template #col-jumlah>
+            <div>Jumlah</div>
+          </template>
+          <template #col-centang>
+            <div>Beli</div>
+          </template>
+          <template #cell-stok="{row}">
+            <div class="row justify-between no-wrap">
+              <div class="q-mr-xs">
+                Gudang
+              </div>
+              <div>
+                {{ row.stokGudang }}
+              </div>
+            </div>
+            <div class="row justify-between no-wrap">
+              <div class="q-mr-xs">
+                Seluruh Rumah Sakit
+              </div>
+              <div>
+                {{ row.stokRS }}
+              </div>
+            </div>
+            <div class="row justify-between no-wrap">
+              <div class="q-mr-xs">
+                Maksimal Rumah Sakit
+              </div>
+              <div>
+                {{ row.stokMaxRS }}
+              </div>
+            </div>
+          </template>
+          <template #cell-pabrikan="{row}">
+            nanti isinya pabrikan
+            <div v-if="row.stokrealgudang.length">
+              {{ row.stokrealgudang[0].jumlah }}
+            </div>
+            <div v-else>
+              -
+            </div>
+          </template>
+          <template #cell-jumlah="{row}">
+            <div class="row justify-between no-wrap">
+              <div class="q-mr-xs">
+                Maksimal dibeli
+              </div>
+              <div>
+                {{ row.bisaBeli }}
+              </div>
+            </div>
+            <div class="row justify-between no-wrap">
+              <app-input
+                v-model="row.jumlahBeli"
+                label="Jumlah Dipesan"
+                :filled="false"
+                :rules="[
+                  val=> (val <= row.bisaBeli) || 'Tidak Boleh Lebih dari Jumlah maksimal dibeli'
+                ]"
+              />
+            </div>
+          </template>
+          <template #cell-centang="{row}">
+            <div v-if="row.bisaBeli>0">
+              <q-checkbox
+                v-model="row.checked"
+                dense
+              />
+            </div>
+            <div v-else>
+              Tidak bisa melakukan pemesanan
+            </div>
+          </template>
+        </app-table>
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 <script setup>
