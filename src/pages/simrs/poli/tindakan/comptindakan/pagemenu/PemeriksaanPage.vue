504522e8d490efea9fae98497aeedc6a93a6e1ba
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-08-15 15:16:36 +0700

    zC

--- src/pages/simrs/poli/tindakan/comptindakan/pagemenu/PemeriksaanPage.vue
+++ src/pages/simrs/poli/tindakan/comptindakan/pagemenu/PemeriksaanPage.vue
@@ -5,59 +5,81 @@
       bordered
       class="full-height"
     >
-      <div class="absolute left-menu">
+      <div class="absolute left-menu invisible">
         <q-list
           padding
           class="rounded-borders text-primary"
         >
           <q-item
-            v-for="n in 10"
-            :key="n"
+            v-for="(n, i) in anats"
+            :key="i"
             v-ripple
             clickable
             active-class="my-menu-link"
-            :active="n===active"
-            @click="active = n"
+            :active="(i)===active"
+            @click="active = i"
           >
             <q-item-section
               avatar
-              @mouseover="hoverred = n"
+              @mouseover="hoverred = i"
               @mouseleave="hoverred = null"
             >
               <div class="column flex-center items-center text-white q-px-xs icon--item">
                 <q-icon
                   class="icon--h"
-                  :class="hoverred===n?'hoverred':''"
-                  name="icon-mat-history"
+                  :class="hoverred===i?'hoverred':''"
+                  :name="n.icon"
                   size="lg"
                 />
-                <div>Inbox</div>
+                <div class="text-center q-mt-xs">
+                  {{ n.name }}
+                </div>
               </div>
             </q-item-section>
           </q-item>
         </q-list>
       </div>
 
-      <div class="absolute right-menu">
+      <div class="absolute right-menu invisible">
         asd
       </div>
-      <div class="column full-height scroll">
-        <BodyPage />
+      <div class="row full-height">
+        <!-- <BodyPage /> -->
+        <div class="col-4">
+          <div class="q-pa-md">
+            <canvas-page />
+          </div>
+        </div>
+        <div class="col-8 bg-red">
+          <div class="q-pa-md" />
+        </div>
       </div>
     </q-card>
   </div>
 </template>
 
 <script setup>
-import BodyPage from './comppemeriksaan/Bodypage.vue'
+// import BodyPage from './comppemeriksaan/Bodypage.vue'
+import CanvasPage from './comppemeriksaan/CanvasPage.vue'
 import { ref } from 'vue'
+
+const anats = ref([
+  { name: 'Body', icon: 'icon-my-human-body-silhouette-with-focus-on-respiratory-system-svgrepo-com' },
+  { name: 'Kepala', icon: 'icon-my-human-skull-side-view-svgrepo-com' },
+  { name: 'Mata', icon: 'icon-mat-visibility' },
+  { name: 'Telinga', icon: 'icon-my-ear-outline-svgrepo-com' },
+  { name: 'Hidung', icon: 'icon-my-nose-outline-svgrepo-com' },
+  { name: 'Rambut', icon: 'icon-my-male-black-short-hair-shape-silhouette-svgrepo-com' },
+  { name: 'Bibir', icon: 'icon-my-thin-lips-outline-svgrepo-com' },
+  { name: 'Gigi Geligi', icon: 'icon-my-tooth-outline-svgrepo-com' }
+])
 
 const active = ref(1)
 const hoverred = ref(null)
 </script>
 <style lang="scss" scoped>
 .left-menu{
-  background-color: rgba($color: $primary, $alpha: 0.8);
+  background-color: rgba($color: $primary, $alpha: 0.9);
   width:80px;
   // height: calc(100% - 30px) !important;
   height:100%;
