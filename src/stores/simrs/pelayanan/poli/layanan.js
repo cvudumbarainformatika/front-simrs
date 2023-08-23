c4c2c886e6c1a984c3415988b0cc7d394cb2e386
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-08-24 00:58:46 +0700

    sada

---
+++ src/stores/simrs/pelayanan/poli/layanan.js
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia'
+// import { api } from 'src/boot/axios'
+
+export const useLayananPoli = defineStore('layanan-poli', {
+  state: () => ({
+    tab: 'Diagnosa',
+    tabs: ['Diagnosa', 'Tindakan']
+  }),
+  // getters: {
+  //   doubleCount: (state) => state.counter * 2
+  // },
+  actions: {
+
+    initReset() {
+      return new Promise((resolve, reject) => {
+        this.tab = 'Diagnosa'
+        this.tabs = ['Diagnosa', 'Tindakan']
+
+        resolve()
+      })
+    }
+
+  }
+})
