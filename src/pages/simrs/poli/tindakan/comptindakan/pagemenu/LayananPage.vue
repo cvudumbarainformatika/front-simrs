93ae8de1f2a7b2eb4af46587c52b39ae6b4585cb
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-08-24 02:06:37 +0700

    fggf

--- src/pages/simrs/poli/tindakan/comptindakan/pagemenu/LayananPage.vue
+++ src/pages/simrs/poli/tindakan/comptindakan/pagemenu/LayananPage.vue
@@ -1,14 +1,62 @@
 <template>
   <div
-    class="full-height q-ma-sm bg-yellow"
+    class="column full-height q-ma-sm"
     style="overflow: hidden;"
   >
-    <q-tabs
+    <div class="column full-height">
+      <div class="col-auto bg-red">
+        <q-tabs
+          v-model="store.tab"
+          no-caps
+          inline-label
+          class="bg-primary text-white shadow-2"
+          align="left"
+          dense
+          active-color="yellow"
+        >
+          <q-tab
+            v-for="(item, i) in store.tabs"
+            :key="i"
+            :name="item"
+            :label="item"
+          />
+        </q-tabs>
+      </div>
+      <div class="col-grow bg-yellow">
+        <q-tab-panels
+          v-model="store.tab"
+          animated
+          class="full-height"
+        >
+          <q-tab-panel
+            name="Diagnosa"
+            class="full-height"
+          >
+            <div class="text-h6">
+              Diagnosa
+            </div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+          <q-tab-panel
+            name="Tindakan"
+            class="full-height"
+          >
+            <div class="text-h6">
+              Tindakan
+            </div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+        </q-tab-panels>
+      </div>
+    </div>
+    <!-- <q-tabs
       v-model="store.tab"
+      no-caps
       inline-label
       class="bg-primary text-white shadow-2"
       align="left"
       dense
+      active-color="yellow"
     >
       <q-tab
         v-for="(item, i) in store.tabs"
@@ -17,13 +65,20 @@
         :label="item"
       />
     </q-tabs>
-    <q-card
-      flat
-      bordered
-      class="full-height"
+    <q-tab-panels
+      v-model="store.tab"
+      animated
     >
-      asd lorem
-    </q-card>
+      <q-tab-panel
+        name="Diagnosa"
+        class="full-height"
+      >
+        <div class="text-h6">
+          Mails
+        </div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+    </q-tab-panels> -->
   </div>
 </template>
 
