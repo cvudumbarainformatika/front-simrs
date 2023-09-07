a2d36b3df96412a40de9adf290f12f535977c446
duro06 <andi.meka.025@gmail.com>
2023-09-07 15:57:46 +0700

    anu

--- src/pages/simrs/farmasi/permintaandepo/permintaan/PermintaanPage.vue
+++ src/pages/simrs/farmasi/permintaandepo/permintaan/PermintaanPage.vue
@@ -1,3 +1,243 @@
 <template>
-  Hlapam Permintaan
+  <!-- head -->
+  <div
+    class="fixed-top row no-wrap items-center justify-between q-mx-sm bg-white"
+    style="z-index: 10;"
+  >
+    <div>
+      <div class="row items-center">
+        <div class="q-mr-md">
+          No Permintaan:
+        </div>
+        <app-input
+          v-model="store.form.nopermintaan"
+          label="Nomor Permintaan"
+          outlined
+          readonly
+          valid
+          :loading="store.loading"
+        />
+        <div class="q-ml-md">
+          <q-btn
+            v-if="store.form.nopermintaan"
+            flat
+            icon="icon-mat-done"
+            dense
+            color="primary"
+            :loading="store.loadingKunci"
+            @click="store.selesaiDanKunci()"
+          >
+            <q-tooltip
+              class="primary"
+              :offset="[10, 10]"
+            >
+              Selesai dan Kunci Penerimaan
+            </q-tooltip>
+          </q-btn>
+        </div>
+      </div>
+    </div>
+    <div>
+      <q-btn
+        flat
+        :icon="!style.componentfull ? 'icon-mat-open_in_full' : 'icon-mat-close_fullscreen'"
+        round
+        :color="style.componentfull ? 'green' : 'primary'"
+        size="12px"
+        class="q-ml-md"
+        @click="style.setComponentFull"
+      />
+    </div>
+  </div>
+  <!-- Permintaan -->
+  <div class="q-mt-lg q-py-md q-px-sm">
+    <div class="row q-col-gutter-md items-center">
+      <div class="col-6">
+        <div class="row q-mb-xs">
+          <div class="col-12">
+            <app-input-date-human
+              :model="store.disp.tanggal"
+              label="Tanggal Transaksi"
+              outlined
+              @set-display="dispTanggal"
+              @db-model="setTanggal"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="row no-wrap q-mb-xs">
+          <div class="col-4">
+            Penanggung Jawab
+          </div>
+          <div class="col-8 text-weight-bold">
+            {{ user.nama }}
+          </div>
+        </div>
+        <div class="row no-wrap q-mb-xs">
+          <div class="col-4">
+            Gudang Asal :
+          </div>
+          <div class="col-4 text-weight-bold">
+            {{ store.disp.depo? store.disp.depo :'-' }}
+          </div>
+          <div class="col-4 text-cyan">
+            ({{ store.form.depo ? store.form.depo :'-' }})
+          </div>
+        </div>
+        <div class="row no-wrap q-mb-xs">
+          <div class="col-4 ">
+            Depo Tujuan :
+          </div>
+          <div class="col-4 text-weight-bold">
+            {{ store.disp.depo? store.disp.depo :'-' }}
+          </div>
+          <div class="col-4 text-cyan">
+            ({{ store.form.depo ? store.form.depo :'-' }})
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="bg-grey-2">
+      <!-- <div
+        class="row bg-grey q-pa-sm"
+      >
+        <div class="f-14 text-weight-bold">
+          Rincian Permintaan
+        </div>
+      </div> -->
+      <div
+        class="row bg-grey q-pa-xs q-mt-sm"
+      >
+        <div class="f-12 text-weight-bold">
+          Rincian Permintaan Obat
+        </div>
+      </div>
+      <div class="row q-my-sm q-col-gutter-md">
+        <div class="col-6">
+          <div class="row q-mb-xs">
+            <div
+              class="col-12"
+            >
+              <CompSelect
+                ref="refGudang"
+                :model="store.form.kdobat"
+                autocomplete="nama_obat"
+                option-label="nama_obat"
+                option-value="kd_obat"
+                label="Cari Obat"
+                outlined
+                :debounce="700"
+                :source="store.obats"
+                :loading="store.loadingObat"
+                @on-select="store.obatSelected"
+                @clear="store.clearObat"
+                @buang="store.cariObat"
+              />
+            </div>
+          </div>
+          <div class="row q-mb-xs">
+            <div class="col-12">
+              <app-input
+                ref="JumlahMinta"
+                v-model="store.form.jumlahminta"
+                label="Jumlah Minta"
+                outlined
+                :rules="[
+                  val => !isNaN(val) || 'Harus pakai Nomor'
+                ]"
+              />
+            </div>
+          </div>
+          <div class="row q-mb-xs q-ml-xs items-center">
+            <div class="q-mr-sm">
+              Status :
+            </div>
+            <div class="q-mr-sm">
+              <q-radio
+                v-model="store.form.konsinyasi"
+                checked-icon="icon-mat-task_alt"
+                unchecked-icon="icon-mat-panorama_fish_eye"
+                val="non-konsinyasi"
+                label="Non-Konsinyasi"
+              />
+            </div>
+            <div class="q-mr-sm">
+              <q-radio
+                v-model="store.form.konsinyasi"
+                checked-icon="icon-mat-task_alt"
+                unchecked-icon="icon-mat-panorama_fish_eye"
+                val="konsinyasi"
+                label="Konsinyasi"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="row q-mb-xs">
+            <div>Kode Barang</div>
+            <div class="q-ml-sm">
+              {{ store.form.kdobat ? store.form.kdobat:'-' }}
+            </div>
+          </div>
+          <div class="row q-mb-xs">
+            gudang asal
+          </div>
+          <div class="row q-mb-xs">
+            Stok user
+          </div>
+          <div class="row q-mb-xs">
+            Stok alokasi
+          </div>
+          <div class="row q-mb-xs">
+            Max Stok
+          </div>
+        </div>
+      </div>
+      <div class="row q-my-sm q-mr-lg justify-end">
+        <app-btn
+          label="Simpan Rincian"
+        />
+      </div>
+      <div
+        class="row bg-grey q-pa-xs"
+      >
+        <div class="f-12 text-weight-bold">
+          Rincian Permintaan Tersimpan
+        </div>
+      </div>
+      <div class="row">
+        anu
+      </div>
+    </div>
+  </div>
 </template>
+<script setup>
+import { useAplikasiStore } from 'src/stores/app/aplikasi'
+import { useStyledStore } from 'src/stores/app/styled'
+import { useFarmasiPermintaanDepoStore } from 'src/stores/simrs/farmasi/permintaandepo/permintaandepo'
+import { computed } from 'vue'
+
+import CompSelect from './comp/CompSelect.vue'
+
+const style = useStyledStore()
+const store = useFarmasiPermintaanDepoStore()
+
+function setTanggal (val) {
+  store.setForm('tanggal', val)
+}
+function dispTanggal (val) {
+  store.setDisp('tanggal', val)
+}
+
+const apps = useAplikasiStore()
+const user = computed(() => {
+  if (apps.user.pegawai) {
+    if (apps.user.pegawai.depo) {
+      store.setForm('depo', apps.user.pegawai.depo.kode)
+      store.setDisp('depo', apps.user.pegawai.depo.nama)
+    }
+  }
+  return apps.user
+})
+</script>
---
+++ src/stores/simrs/farmasi/permintaandepo/permintaandepo.js
@@ -0,0 +1,112 @@
+import { defineStore } from 'pinia'
+import { date } from 'quasar'
+
+export const useFarmasiPermintaanDepoStore = defineStore('fermasi_permintaan_depo', {
+  state: () => ({
+    loading: false,
+    loadingKunci: false,
+    loadingObat: false,
+    form: {
+      tanggal: date.formatDate(Date.now(), 'DD MMMM YYYY'),
+      konsinyasi: 'non-konsinyasi'
+    },
+    disp: {
+      tanggal: date.formatDate(Date.now(), 'DD MMMM YYYY')
+    },
+    floor: [ // racikan
+      { kode: 'Gd-03010101' },
+      { kode: 'Gd-04010101' }
+    ],
+    obats: [
+      {
+        id: 20,
+        kd_obat: '0000037-FAR',
+        nama_obat: 'DIGOXIN 0,25 MG/ML 2 ML INJEKSI',
+        merk: 'MEPROFARM',
+        kandungan: 'Digoksin',
+        jenis_perbekalan: 'Obat',
+        bentuk_sediaan: 'INJEKSI',
+        kode108: '1.1.7.01.04.01.001',
+        uraian108: 'Obat Cair',
+        kode50: '5.1.02.01.01.037',
+        uraian50: 'BELANJA OBAT-OBATAN',
+        satuan_b: 'Box',
+        satuan_k: 'AMPUL',
+        kelompok_psikotropika: '0',
+        kelompok_penyimpanan: 'INJEKSI',
+        kelompok_rko: 'Digoksin inj 0,25 mg/ml',
+        status_generik: '1',
+        status_forkid: '1',
+        status_fornas: '1',
+        kekuatan_dosis: '0,25 MG/ML',
+        volumesediaan: '2 ML',
+        kelas_terapi: '',
+        nilai_kdn: 37.05,
+        sertifikatkdn: '4956/SJ-IND.8/TKDN/10/2022',
+        sistembayar: 'SEMUA',
+        updated_at: '2023-09-06 10:41:37',
+        created_at: '2023-09-04 15:16:56',
+        deleted_at: null,
+        stok: 20
+      },
+      {
+        id: 21,
+        kd_obat: '0000038-FAR',
+        nama_obat: 'ENOXRIN 4000 INJEKSI',
+        merk: 'LAPI LABORATORIES',
+        kandungan: 'Enoksaparin sodium',
+        jenis_perbekalan: 'Obat',
+        bentuk_sediaan: 'INJEKSI',
+        kode108: '1.1.7.01.04.01.001',
+        uraian108: 'Obat Cair',
+        kode50: '5.1.02.01.01.037',
+        uraian50: 'BELANJA OBAT-OBATAN',
+        satuan_b: 'Box',
+        satuan_k: 'Prefilled Syringe',
+        kelompok_psikotropika: '0',
+        kelompok_penyimpanan: 'INJEKSI',
+        kelompok_rko: 'Enoksaparin sodium inj 40 mg/0,4 ml',
+        status_generik: '0',
+        status_forkid: '1',
+        status_fornas: '1',
+        kekuatan_dosis: '10000 IU/ML',
+        volumesediaan: '0,4 ML',
+        kelas_terapi: '',
+        nilai_kdn: 0.00,
+        sertifikatkdn: 'IMPORT',
+        sistembayar: 'SEMUA',
+        updated_at: '2023-09-05 05:33:56',
+        created_at: '2023-09-05 05:33:56',
+        deleted_at: null,
+        stok: 10
+      }
+
+    ]
+  }),
+  actions: {
+    setForm(key, val) {
+      this.form[key] = val
+    },
+    setDisp(key, val) {
+      this.disp[key] = val
+    },
+    obatSelected(val) {
+      this.setForm('kdobat', val)
+      console.log('obat ', val)
+      const anu = this.obats.filter(a => a.kd_obat === val)
+      if (anu.length) {
+        const obat = anu[0]
+        console.log('obat ketemu', obat)
+        // this.setForm()
+      }
+    },
+    clearObat(val) {
+      this.setForm('kdobat', null)
+    },
+    cariObat(val) {
+      console.log('cari obat ', val)
+    },
+    getInitialData() {},
+    selesaiDanKunci() {}
+  }
+})
