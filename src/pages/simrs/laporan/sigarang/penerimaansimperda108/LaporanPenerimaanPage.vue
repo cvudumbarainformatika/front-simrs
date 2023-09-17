34e37ae605c590e6a05eb5cbf2870227cc1d3537
duro06 <andi.meka.025@gmail.com>
2023-09-18 06:44:22 +0700

    jhjgg

--- src/pages/simrs/laporan/sigarang/penerimaansimperda108/LaporanPenerimaanPage.vue
+++ src/pages/simrs/laporan/sigarang/penerimaansimperda108/LaporanPenerimaanPage.vue
@@ -1,5 +1,14 @@
 <template>
-  Laporan penerimaan simperda 108
+  <div class="q-pa-sm">
+    <div
+      class="row bg-grey-4 q-pa-sm q-mb-sm"
+    >
+      <div class="f-14 text-weight-bold">
+        Laporan Penerimaan Simperda 108
+      </div>
+    </div>
+    tabel
+  </div>
 </template>
 <script setup>
 import { useSimrsLaporanSigarangPenerimaanSimperda108Store } from 'src/stores/simrs/laporan/sigarang/penerimaansimperda108/penerimaan'
--- src/pages/simrs/laporan/sigarang/penerimaansimperdarinci/LaporanPenerimaanPage.vue
+++ src/pages/simrs/laporan/sigarang/penerimaansimperdarinci/LaporanPenerimaanPage.vue
@@ -1,5 +1,14 @@
 <template>
-  Laporan penerimaan simperda rinci
+  <div class="q-pa-sm">
+    <div
+      class="row bg-grey-4 q-pa-sm q-mb-sm"
+    >
+      <div class="f-14 text-weight-bold">
+        Laporan Penerimaan Simperda Rinci
+      </div>
+    </div>
+    tabel
+  </div>
 </template>
 <script setup>
 import { useSimrsLaporanSigarangPenerimaanSimperdaRinciStore } from 'src/stores/simrs/laporan/sigarang/penerimaansimperdarinci/penerimaan'
