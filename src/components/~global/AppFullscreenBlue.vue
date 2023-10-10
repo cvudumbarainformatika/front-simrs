675d984a6a35ae1817c86010c803195974ebe985
duro06 <andi.meka.025@gmail.com>
2023-10-10 16:01:28 +0700

    blue

---
+++ src/components/~global/AppFullscreenBlue.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-dialog
+    persistent
+    maximized
+    transition-show="slide-left"
+    transition-hide="fade"
+  >
+    <q-card flat>
+      <q-bar>
+        <q-space />
+
+        <q-btn
+          v-close-popup
+          dense
+          flat
+          icon="icon-mat-close"
+          @click="emits('close')"
+        >
+          <q-tooltip class="bg-white text-primary">
+            Close
+          </q-tooltip>
+        </q-btn>
+      </q-bar>
+
+      <q-card-section>
+        <slot />
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+// import { ref } from 'vue'
+
+// const maximizedToggle = ref(true)
+const emits = defineEmits(['close'])
+</script>
