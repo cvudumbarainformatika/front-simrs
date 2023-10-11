debf834df595c4cbb26e19785a82227b22aa7d31
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-10-11 14:22:40 +0700

    cxdzvfxdc

--- src/stores/simrs/pelayanan/poli/edukasi.js
+++ src/stores/simrs/pelayanan/poli/edukasi.js
@@ -1,15 +1,42 @@
 import { defineStore } from 'pinia'
+import { api } from 'src/boot/axios'
 
 export const useEdukasiPoliStore = defineStore('edukasi-poli', {
   state: () => ({
-    counter: 0
+    mpenerimaedukasi: [],
+    mkebutuhanedukasi: [],
+    selectKebutuhanEdukasi: [],
+    form: {
+      noreg: '',
+      norm: '',
+      edukasi: '',
+      koderuang: '', // kodepoli
+      kepada: '',
+      perlupenerjemah: 'Tidak',
+      bahasaisyarat: 'Tidak',
+      caraedukasi: '',
+      kesediaan: '',
+      kebutuhanedukasi: ''
+    },
+    loadingSave: false
   }),
   getters: {
     doubleCount: (state) => state.counter * 2
   },
   actions: {
-    getMaster() {
-      //
+    async getMasterPenerimaEdukasi() {
+      const resp = await api.get('v1/simrs/pelayanan/mpenerimaedukasi')
+      console.log('mpenerimaedukasi', resp)
+      if (resp?.status === 200) {
+        this.mpenerimaedukasi = resp?.data
+      }
+    },
+    async getMasterKebutuhanEdukasi() {
+      const resp = await api.get('v1/simrs/pelayanan/mkebutuhanedukasi')
+      console.log('mkebutuhanedukasi', resp)
+      if (resp?.status === 200) {
+        this.mkebutuhanedukasi = resp?.data
+      }
     }
   }
 })
