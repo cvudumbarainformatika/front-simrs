5f1e1b0df07678eeb3cfee2e20ec10c3e7c27f3f
duro06 <andi.meka.025@gmail.com>
2023-10-14 12:05:18 +0700

    pengeluaran

--- src/pages/simrs/laporan/sigarang/pengeluaran/PengeluaranPage.vue
+++ src/pages/simrs/laporan/sigarang/pengeluaran/PengeluaranPage.vue
@@ -1,6 +1,340 @@
 <template>
-  <div class="row justify-center">
-    <div>Halaman Pegeluaran</div>
+  <div class="bg-white q-pa-xs">
+    <div
+      class="row bg-primary text-white q-pa-sm q-mb-sm"
+    >
+      <div class="f-14 text-weight-bold">
+        Laporan Pengeluaran Depo
+      </div>
+    </div>
+    <div class="row items-center garis-bawah">
+      <div class="col-2">
+        <q-img
+          src="~assets/images/logo-kota-grey.png"
+          spinner-color="white"
+          style="height: 3.56cm; max-width: 2.86cm"
+        />
+      </div>
+      <div class="col-8">
+        <div class="row justify-center text-center f-18">
+          PEMERINTAH KOTA PROBOLINGGO
+        </div>
+        <div class="row justify-center text-center f-12 text-weight-bold">
+          DINAS KESEHATAN, PENGENDALIAN PENDUDUK, DAN KELUARGA BERENCANA
+        </div>
+        <div class="row justify-center text-center f-20 text-weight-bold">
+          UOBK RSUD DOKTER MOHAMAD SALEH
+        </div>
+        <div class="row justify-center text-center f-12">
+          Jl. Mayjen Panjaitan No.65 Telp.(0335) 433119, 42118 Fax (0335) 432702
+        </div>
+        <div class="row justify-center text-center f-14">
+          E-mail : rsudprob@probolinggokota.go.id
+        </div>
+        <div class="row justify-center text-center f-14 text-weight-bold">
+          PROBOLINGGO  67219
+        </div>
+      </div>
+      <div class="col-2">
+        <q-img
+          src="~assets/logos/logo-rsud.png"
+          spinner-color="white"
+          style="height: 3cm; max-width: 3cm"
+        />
+      </div>
+    </div>
+
+    <div class="row justify-center f-16 text-weight-bold q-my-sm">
+      Laporan Pengeluaran Depo
+    </div>
+    <div class="row justify-center f-12 text-weight-bold q-my-sm">
+      Periode {{ store.display.from + ' - ' + store.display.to }}
+    </div>
+    <div class="row q-col-gutter-sm q-my-sm">
+      <div class="col-2">
+        <app-input-date-human
+          :model="store.display.from"
+          label="Dari tanggal"
+          outlined
+          @db-model="setDari"
+          @set-display="setDispDari"
+        />
+      </div>
+      <div class="col-2">
+        <app-input-date-human
+          :model="store.display.to"
+          label="Sampai tanggal"
+          outlined
+          @db-model="setKe"
+          @set-display="setDispKe"
+        />
+      </div>
+    </div>
+    <div class="row q-col-gutter-sm q-my-sm">
+      <div class="col-4">
+        <app-autocomplete
+          v-model="store.params.kode_ruang"
+          label="pilih Depo"
+          autocomplete="nama"
+          option-label="nama"
+          option-value="value"
+          valid
+          outlined
+          :source="store.gudangs"
+          :loading="store.loading"
+        />
+      </div>
+      <div class="col-2">
+        <app-btn
+          label="Ambil Data"
+          :disable="store.loading"
+          :loading="store.loading"
+          @click="store.getDataTable"
+        />
+      </div>
+    </div>
+    <app-table-extend
+      id="printMe"
+      :columns="store.columns"
+      :items="store.items"
+      :meta="store.meta"
+      :per-page="store.params.per_page"
+      :loading="store.loading"
+      :to-search="store.params.q"
+      :default-btn="false"
+      :ada-tambah="false"
+      :ada-filter="false"
+      text-cari="Cari Nama barang..."
+      row-no
+      bottom-row
+      tanda-tangan
+      @find="store.setSearch"
+      @goto="store.setPage"
+      @set-row="store.setPerPage"
+      @refresh="store.refreshTable"
+    >
+      <template #header-for-print>
+        <div class="row items-center garis-bawah">
+          <div class="col-2">
+            <q-img
+              src="~assets/images/logo-kota-grey.png"
+              spinner-color="white"
+              style="height: 3.56cm; max-width: 2.86cm"
+            />
+          </div>
+          <div class="col-8">
+            <div class="row justify-center text-center f-18">
+              PEMERINTAH KOTA PROBOLINGGO
+            </div>
+            <div class="row justify-center text-center f-12 text-weight-bold">
+              DINAS KESEHATAN, PENGENDALIAN PENDUDUK, DAN KELUARGA BERENCANA
+            </div>
+            <div class="row justify-center text-center f-20 text-weight-bold">
+              UOBK RSUD DOKTER MOHAMAD SALEH
+            </div>
+            <div class="row justify-center text-center f-12">
+              Jl. Mayjen Panjaitan No.65 Telp.(0335) 433119, 42118 Fax (0335) 432702
+            </div>
+            <div class="row justify-center text-center f-14">
+              E-mail : rsudprob@probolinggokota.go.id
+            </div>
+            <div class="row justify-center text-center f-14 text-weight-bold">
+              PROBOLINGGO  67219
+            </div>
+          </div>
+          <div class="col-2">
+            <q-img
+              src="~assets/logos/logo-rsud.png"
+              spinner-color="white"
+              style="height: 3cm; max-width: 3cm"
+            />
+          </div>
+        </div>
+
+        <div class="row justify-center f-16 text-weight-bold q-my-sm">
+          Laporan Pengeluaran Depo
+        </div>
+        <div class="row justify-center f-12 text-weight-bold q-my-sm">
+          Periode {{ store.display.from + ' - ' + store.display.to }}
+        </div>
+      </template>
+      <template #header-right-before>
+        <q-btn
+          ref="refPrint"
+          v-print="printObj"
+          unelevated
+          color="dark"
+          round
+          size="sm"
+          icon="icon-mat-print"
+        >
+          <q-tooltip
+            class="primary"
+            :offset="[10, 10]"
+          >
+            Print
+          </q-tooltip>
+        </q-btn>
+      </template>
+      <template #col-tanggal>
+        <div>Tanggal</div>
+      </template>
+      <template #col-tujuan>
+        <div>Tujuan</div>
+      </template>
+      <template #col-satuan>
+        <div>Satuan</div>
+      </template>
+      <template #col-jumlah>
+        <div>Qty</div>
+      </template>
+      <template #col-jumlah_disetujui>
+        <div>Qty Disetujui</div>
+      </template>
+      <template #col-jumlah_distribusi>
+        <div>Qty Distribusi</div>
+      </template>
+      <template #col-kode_rs>
+        <div>Kode</div>
+      </template>
+      <template #col-nama>
+        <div>Nama</div>
+      </template>
+      <template #cell-satuan="{row}">
+        <div>{{ row.satuan }}</div>
+      </template>
+      <template #cell-tanggal="{row}">
+        <div>{{ row.tanggal?dateFullFormat(row.tanggal):dateFullFormat(row.tanggal_l) }}</div>
+      </template>
+      <template #cell-jumlah="{row}">
+        <div class="text-right">
+          {{ row.jumlah??0 }}
+        </div>
+      </template>
+      <template #cell-jumlah_disetujui="{row}">
+        <div class="text-right">
+          {{ row.jumlah_disetujui??0 }}
+        </div>
+      </template>
+      <template #cell-jumlah_distribusi="{row}">
+        <div class="text-right">
+          {{ row.jumlah_distribusi??row.jumlah_distribusi_l }}
+        </div>
+      </template>
+      <template #cell-kode_rs="{row}">
+        <div>
+          {{ row.kode_rs ?? row.kode_rs_l }}
+        </div>
+      </template>
+      <template #cell-status="{row}">
+        <div
+          v-if="row.status<=1"
+          class="text-negative text-weight-bold"
+        >
+          Belum Diterima gudang
+        </div>
+        <div
+          v-if="row.status>=2 && row.sisa_stok > 0"
+          class="text-deep-orange text-weight-bold"
+        >
+          Sudah di Gudang
+        </div>
+        <div
+          v-if="row.status>=2 && row.sisa_stok <= 0"
+          class="text-green"
+        >
+          Sudah di distribusikan
+        </div>
+      </template>
+      <template #bottom-row>
+        <td colspan="8">
+          <div class="text-right f-12">
+            Jumlah
+          </div>
+        </td>
+        <td>
+          <div
+            v-if="store.items.length"
+            class="text-right f-12"
+          >
+            {{ store.total }}
+            <!-- {{ store.items.map(anu=>anu.subtotal) }} -->
+          </div>
+        </td>
+      </template>
+    </app-table-extend>
   </div>
-  <app-maintenance />
 </template>
+<script setup>
+import { dateFullFormat } from 'src/modules/formatter'
+import { useLaporanSigarangPengeluaranStore } from 'src/stores/simrs/laporan/sigarang/pengeluaran/pengeluaran'
+
+const store = useLaporanSigarangPengeluaranStore()
+store.getInitialData()
+
+function setDari(val) {
+  store.setParams('from', val)
+}
+function setDispDari(val) {
+  store.display.from = val
+}
+function setKe(val) {
+  store.setParams('to', val)
+}
+function setDispKe(val) {
+  store.display.to = val
+}
+const printObj = {
+  id: 'printMe',
+  popTitle: 'Laporan Persediaan FiFo'
+  // extraCss: 'https://cdn.bootcdn.net/ajax/libs/animate.css/4.1.1/animate.compat.css, https://cdn.bootcdn.net/ajax/libs/hover.css/2.3.1/css/hover-min.css',
+  // extraHead: '<meta http-equiv="Content-Language"content="zh-cn"/>',
+
+}
+</script>
+<style lang="scss" scoped>
+$fs : 9px;
+.app-table {
+  width: 100%; /* print width */
+  font-size:$fs;
+
+  .q-table td {
+    padding-left: 10px;
+    font-size: $fs;
+  }
+  .q-table th {
+    padding-left: 10px;
+    font-size: $fs;
+  }
+}
+
+@media print {
+  .app-table {
+    width: 100%; /* print width */
+    font-size:$fs;
+
+    .q-table {
+        max-width: 100% !important;
+      }
+    .q-table td {
+      padding: 2px;
+      font-size: $fs;
+       white-space: normal !important;
+        word-wrap: normal !important;
+        hyphens: manual;
+    }
+    .q-table th {
+      padding:2px;
+      font-size:$fs;
+      white-space: normal !important;
+        word-wrap: normal !important;
+        hyphens: manual;
+    }
+
+    .screenwide{
+      max-width: 100% !important;
+    }
+  }
+}
+
+</style>
---
+++ src/stores/simrs/laporan/sigarang/pengeluaran/pengeluaran.js
@@ -0,0 +1,99 @@
+import { defineStore } from 'pinia'
+import { date } from 'quasar'
+import { api } from 'src/boot/axios'
+
+export const useLaporanSigarangPengeluaranStore = defineStore('laporan_sigarang_pengeluaran', {
+  state: () => ({
+    loading: false,
+    items: [],
+    meta: {},
+    params: {
+      q: '',
+      page: 1,
+      per_page: 10,
+      from: date.formatDate(Date.now(), 'YYYY-MM-01'),
+      to: date.formatDate(Date.now(), 'YYYY-MM-DD'),
+      kode_ruang: '',
+      kode_rs: null
+    },
+    display: {
+      from: date.formatDate(Date.now(), '01 MMMM YYYY'),
+      to: date.formatDate(Date.now(), 'DD MMMM YYYY')
+    },
+    gudangs: [
+      { nama: 'Semua Depo', value: '' },
+      { nama: 'Depo PNM', value: 'Gd-02010101' },
+      { nama: 'Depo Gizi', value: 'Gd-02010102' },
+      { nama: 'Depo Habis Pakai', value: 'Gd-02010103' }
+    ],
+    barangs: [],
+    columns: [
+      'tanggal',
+      'tujuan',
+      'kode_rs',
+      'nama',
+      'satuan',
+      'jumlah',
+      'jumlah_disetujui',
+      'jumlah_distribusi'
+    ],
+    total: 0
+  }),
+  actions: {
+    setParams(key, val) {
+      this.params[key] = val
+    },
+    setSearch(payload) {
+      this.setParams('q', payload)
+      this.setParams('page', 1)
+      this.getDataTable()
+    },
+    setPage(payload) {
+      this.setParams('page', payload)
+      this.getDataTable()
+    },
+    setPerPage(payload) {
+      this.setParams('per_page', payload)
+      this.setParams('page', 1)
+      this.getDataTable()
+    },
+    refreshTable() {
+      this.setParams('page', 1)
+      this.getDataTable()
+    },
+    getInitialData() {
+      this.getDataTable()
+      // this.getBarang()
+    },
+    async getDataTable() {
+      this.loading = true
+      const param = { params: this.params }
+      await api.get('v1/simrs/laporan/sigarang/pengeluaran-depo', param)
+        .then(resp => {
+          this.loading = false
+          console.log('data tabel', resp.data)
+          this.meta = resp.data
+          this.items = resp.data.data
+          this.total = this.items.map(a => {
+            if (a.jumlah_distribusi > 0) {
+              return a.jumlah_distribusi
+            } else if (a.jumlah_distribusi_l > 0) {
+              return a.jumlah_distribusi_l
+            } else { return 0 }
+          }).reduce((a, b) => a + b, 0)
+        })
+        .catch(() => { this.loading = false })
+    },
+    async getBarang() {
+      this.loading = true
+      const param = { params: this.params }
+      await api.get('v1/simrs/laporan/sigarang/ruangan/barang', param)
+        .then(resp => {
+          this.loading = false
+          console.log('data barang', resp.data)
+          // this.meta = resp.data
+        })
+        .catch(() => { this.loading = false })
+    }
+  }
+})
