6764e1bf52c38a801fab77c0a3c0df02ae20fec2
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2023-12-12 09:56:45 +0700

    dasfdsf

--- src/pages/simrs/ranap/layanan/PageLayananRanap.vue
+++ src/pages/simrs/ranap/layanan/PageLayananRanap.vue
@@ -9,13 +9,28 @@
     <q-card
       square
       flat
+      class="container-no-header"
     >
-      {{ pasien }}
+      <q-layout
+        view="lHr Lpr lFf"
+        container
+        class="shadow-2 rounded-borders z-top"
+      >
+        <q-header
+          elevated
+          class="bg-primary"
+        >
+          <HeaderLayout
+            :pasien="pasien"
+          />
+        </q-header>
+      </q-layout>
     </q-card>
   </q-dialog>
 </template>
 
 <script setup>
+import HeaderLayout from './layoutcomp/HeaderLayout.vue'
 
 defineProps({
   pasien: {
