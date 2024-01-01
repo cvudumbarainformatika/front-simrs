bf37295a7d1d9d8c8b445b3a7f425d3a9841184b
duro06 <andi.meka.025@gmail.com>
2024-01-01 09:39:09 +0700

    resep

--- src/stores/simrs/farmasi/resepdepo/formresepdepo.js
+++ src/stores/simrs/farmasi/resepdepo/formresepdepo.js
@@ -61,7 +61,7 @@ export const useResepDepoFarmasiStore = defineStore('resep_depo_farmasi_setore',
       console.log('pasien', val)
       console.log('jenis pasien', this.jenispasien)
       const temp = val?.diagnosa?.map(x => x?.rs3 + ' - ' + x?.masterdiagnosa?.rs4)
-      const diag = temp.join(',')
+      const diag = temp?.length ? temp.join(',') : '-'
       console.log('diag', diag)
       if (val) {
         this.pasien = val
