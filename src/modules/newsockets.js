96019af51aae72db224cbbe2f6b5726df540a25b
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-01-21 11:30:01 +0700

    sdfg

--- src/modules/newsockets.js
+++ src/modules/newsockets.js
@@ -1,13 +1,13 @@
 import Echo from 'laravel-echo'
 import Pusher from 'pusher-js'
-import { SERV, HOST } from 'src/boot/axios'
+import { SERV, WSHOST, WSPORT } from 'src/boot/axios'
 window.Pusher = Pusher
 
 // const host = '192.168.101.80'
 // const serv = 'http://' + host + '/api.laborat/public'
 // const host = '192.168.150.111'
 // const serv = 'http://' + host + ':3507'
-const port = 6003
+// const port = 6003
 
 const getLocalToken = () => JSON.parse(localStorage.getItem('token'))
 
@@ -15,8 +15,8 @@ window.Echo = new Echo({
   broadcaster: 'pusher',
   key: 'simrs_key_harry141312',
   cluster: 'mt1',
-  wsHost: HOST,
-  wsPort: port,
+  wsHost: WSHOST,
+  wsPort: WSPORT,
   forceTLS: false,
   disableStats: true,
   authEndpoint: SERV + '/api/broadcasting/auth',
