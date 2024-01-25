2af6171567948618a00e330274088c7b7af7b568
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-01-25 09:35:20 +0700

    sdfsd

--- src/pages/simrs/poli/tindakan/comptindakan/pagemenu/comppemeriksaanbaru/ListPemeriksaan.vue
+++ src/pages/simrs/poli/tindakan/comptindakan/pagemenu/comppemeriksaanbaru/ListPemeriksaan.vue
@@ -265,7 +265,7 @@
                       </q-item-label>
                     </q-item-section>
                   </q-item>
-                  <!-- KHUSUS PARU POL018-->
+                  <!-- ========================================================================================== KHUSUS PARU POL018-->
                   <q-separator
                     dark
                     inset
