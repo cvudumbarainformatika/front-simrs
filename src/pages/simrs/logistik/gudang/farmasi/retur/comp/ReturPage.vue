cb685b1d48fbd020a79e9c651cdf7194729bda09
duro06 <andi.meka.meka.025@gmail.com>
2024-04-25 11:52:46 +0700

    simpan retur

--- src/pages/simrs/logistik/gudang/farmasi/retur/comp/ReturPage.vue
+++ src/pages/simrs/logistik/gudang/farmasi/retur/comp/ReturPage.vue
@@ -2,178 +2,169 @@
   <div class="q-pa-sm">
     <!-- head form -->
     <div>
-      <!-- Perusahaan -->
-      <div class="row q-col-gutter-sm items-center q-mb-sm">
-        <div class="col-2">
-          Penyedia
+      <div class="row items-center q-col-gutter-sm">
+        <div class="col-6">
+          <!-- Perusahaan -->
+          <div class="row q-col-gutter-sm items-center q-mb-sm">
+            <div class="col-3">
+              Penyedia
+            </div>
+
+            <div class="col-9">
+              <app-autocomplete-new
+                v-model="store.form.kdpbf"
+                label="Pilih Perusahaan"
+                debounce="700"
+                autocomplete="namaperusahaan"
+                option-label="namaperusahaan"
+                option-value="kodeperusahaan"
+                outlined
+                valid
+                :loading="store.loadingPerusahaan"
+                :source="store.perusahaans"
+                @on-select="store.perusahaanSelected"
+              />
+            </div>
+          </div>
+          <!-- Obat -->
+          <div class="row q-col-gutter-sm items-center q-mb-sm">
+            <div class="col-3">
+              Obat
+            </div>
+
+            <div class="col-9">
+              <app-autocomplete-new
+                ref="refObat"
+                v-model="store.form.kd_obat"
+                label="Pilih Obat"
+                autocomplete="nama_obat"
+                option-label="nama_obat"
+                option-value="kd_obat"
+                outlined
+                valid
+                :loading="store.loadingObat"
+                :source="store.obats"
+                @on-select="store.obatSelected"
+              />
+            </div>
+          </div>
+          <!-- Satuan -->
+          <div class="row q-col-gutter-sm items-center q-mb-sm">
+            <div class="col-3">
+              Satuan
+            </div>
+
+            <div class="col-9">
+              <app-input
+                v-model="store.form.satuan_k"
+                label="Satuan"
+                outlined
+                valid
+                readonly
+              />
+            </div>
+          </div>
+          <!-- No Retur -->
+          <div class="row q-col-gutter-sm items-center q-mb-sm">
+            <div class="col-3">
+              Nomor Retur RS
+            </div>
+
+            <div class="col-9">
+              <app-input
+                v-model="store.form.no_retur"
+                label="Nomor Retur"
+                outlined
+                valid
+                readonly
+              />
+            </div>
+          </div>
+          <!-- tgl Retur -->
+          <div class="row q-col-gutter-sm items-center q-mb-sm">
+            <div class="col-3">
+              Tanggal Retur RS
+            </div>
+
+            <div class="col-9">
+              <app-input-date-human
+                ref="refTaRetur"
+                :model="store.form.tanggal"
+                label="Tanggal Retur"
+                outlined
+                @set-display="setTanggalDisp"
+                @db-model="setTanggal"
+              />
+            </div>
+          </div>
         </div>
-        <div class="col-1">
-          :
-        </div>
-        <div class="col-9">
-          <app-autocomplete-new
-            v-model="store.form.kdpbf"
-            label="Pilih Perusahaan"
-            debounce="700"
-            autocomplete="namaperusahaan"
-            option-label="namaperusahaan"
-            option-value="kodeperusahaan"
-            outlined
-            valid
-            :loading="store.loadingPerusahaan"
-            :source="store.perusahaans"
-            @on-select="store.perusahaanSelected"
-          />
-        </div>
-      </div>
-      <!-- Obat -->
-      <div class="row q-col-gutter-sm items-center q-mb-sm">
-        <div class="col-2">
-          Obat
-        </div>
-        <div class="col-1">
-          :
-        </div>
-        <div class="col-9">
-          <app-autocomplete-new
-            ref="refObat"
-            v-model="store.form.kd_obat"
-            label="Pilih Obat"
-            autocomplete="nama_obat"
-            option-label="nama_obat"
-            option-value="kd_obat"
-            outlined
-            valid
-            :loading="store.loadingObat"
-            :source="store.obats"
-            @on-select="store.obatSelected"
-          />
-        </div>
-      </div>
-      <!-- Satuan -->
-      <div class="row q-col-gutter-sm items-center q-mb-sm">
-        <div class="col-2">
-          Satuan
-        </div>
-        <div class="col-1">
-          :
-        </div>
-        <div class="col-9">
-          <app-input
-            v-model="store.form.satuan_k"
-            label="Satuan"
-            outlined
-            readonly
-          />
-        </div>
-      </div>
-      <!-- No Retur -->
-      <div class="row q-col-gutter-sm items-center q-mb-sm">
-        <div class="col-2">
-          Nomor Retur RS
-        </div>
-        <div class="col-1">
-          :
-        </div>
-        <div class="col-9">
-          <app-input
-            v-model="store.form.no_retur"
-            label="Nomor Retur"
-            outlined
-            readonly
-          />
-        </div>
-      </div>
-      <!-- tgl Retur -->
-      <div class="row q-col-gutter-sm items-center q-mb-sm">
-        <div class="col-2">
-          Tanggal Retur RS
-        </div>
-        <div class="col-1">
-          :
-        </div>
-        <div class="col-9">
-          <app-input-date-human
-            ref="refTaRetur"
-            :model="store.form.tanggal"
-            label="Tanggal Retur"
-            outlined
-            @set-display="setTanggalDisp"
-            @db-model="setTanggal"
-          />
-        </div>
-      </div>
-      <!-- No Faktur Retur -->
-      <div class="row q-col-gutter-sm items-center q-mb-sm">
-        <div class="col-2">
-          Nomor Faktur Retur PBF
-        </div>
-        <div class="col-1">
-          :
-        </div>
-        <div class="col-9">
-          <app-input
-            ref="refNoFakRet"
-            v-model="store.form.no_faktur_retur_pbf"
-            label="Nomor Faktur Retur"
-            valid
-            outlined
-          />
-        </div>
-      </div>
-      <!-- tgl Faktur Retur -->
-      <div class="row q-col-gutter-sm items-center q-mb-sm">
-        <div class="col-2">
-          Tanggal Faktur Retur PBF
-        </div>
-        <div class="col-1">
-          :
-        </div>
-        <div class="col-9">
-          <app-input-date-human
-            ref="refTaFakRetur"
-            :model="store.form.tanggalFak"
-            label="Tanggal Faktur Retur"
-            outlined
-            valid
-            @set-display="setTanggalFakDisp"
-            @db-model="setTanggalFak"
-          />
-        </div>
-      </div>
-      <div class="row q-col-gutter-sm items-center q-mb-sm">
-        <div class="col-2">
-          Tanggal Kwitansi Pembayaran
-        </div>
-        <div class="col-1">
-          :
-        </div>
-        <div class="col-9">
-          <app-input-date-human
-            ref="refTaKwiRetur"
-            :model="store.form.tanggalKwi"
-            label="Tanggal Kwitansi"
-            outlined
-            @set-display="setTanggalKwiDisp"
-            @db-model="setTanggalKwi"
-          />
-        </div>
-      </div>
-      <!-- No Kwitansi Pembayaran -->
-      <div class="row q-col-gutter-sm items-center q-mb-sm">
-        <div class="col-2">
-          Nomor Kwitansi Pembayaran
-        </div>
-        <div class="col-1">
-          :
-        </div>
-        <div class="col-9">
-          <app-input
-            ref="refNoKwiRet"
-            v-model="store.form.no_kwitansi_pembayaran"
-            label="Nomor Kwitansi "
-            outlined
-          />
+        <div class="col-6">
+          <!-- No Faktur Retur -->
+          <div class="row q-col-gutter-sm items-center q-mb-sm">
+            <div class="col-3">
+              Nomor Faktur Retur PBF
+            </div>
+
+            <div class="col-9">
+              <app-input
+                ref="refNoFakRet"
+                v-model="store.form.no_faktur_retur_pbf"
+                label="Nomor Faktur Retur"
+                valid
+                outlined
+              />
+            </div>
+          </div>
+          <!-- tgl Faktur Retur -->
+          <div class="row q-col-gutter-sm items-center q-mb-sm">
+            <div class="col-3">
+              Tanggal Faktur Retur PBF
+            </div>
+
+            <div class="col-9">
+              <app-input-date-human
+                ref="refTaFakRetur"
+                :model="store.form.tanggalFak"
+                label="Tanggal Faktur Retur"
+                outlined
+                valid
+                @set-display="setTanggalFakDisp"
+                @db-model="setTanggalFak"
+              />
+            </div>
+          </div>
+          <!-- tgl Kwitansi Pembayaran -->
+          <div class="row q-col-gutter-sm items-center q-mb-sm">
+            <div class="col-3">
+              Tanggal Kwitansi Pembayaran
+            </div>
+
+            <div class="col-9">
+              <app-input-date-human
+                ref="refTaKwiRetur"
+                :model="store.form.tanggalKwi"
+                label="Tanggal Kwitansi"
+                outlined
+                @set-display="setTanggalKwiDisp"
+                @db-model="setTanggalKwi"
+              />
+            </div>
+          </div>
+          <!-- No Kwitansi Pembayaran -->
+          <div class="row q-col-gutter-sm items-center q-mb-sm">
+            <div class="col-3">
+              Nomor Kwitansi Pembayaran
+            </div>
+
+            <div class="col-9">
+              <app-input
+                ref="refNoKwiRet"
+                v-model="store.form.no_kwitansi_pembayaran"
+                label="Nomor Kwitansi "
+                outlined
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -181,7 +172,7 @@
     <app-loading v-if="store?.loadingDataMauRet" />
     <!-- Daftar barang baik -->
     <div v-if="!store?.loadingDataMauRet">
-      <div class="bg-grey q-pa-sm text-white f-14 q-mb-md">
+      <div class="row bg-grey q-pa-sm text-white f-14 q-mb-sm">
         Barang Diterima
       </div>
       <div v-if="!store?.dataMauReturs?.length">
@@ -189,7 +180,20 @@
       </div>
       <!-- <div> -->
       <div v-if="store?.dataMauReturs?.length">
-        <div class="row bg-dark text-white q-col-gutter-sm q-mb-sm">
+        <div class="row q-mb-sm">
+          <app-input
+            ref="refCariNoPenerimaan"
+            v-model="store.params.nopenerimaan"
+            label="Cari Nomor Penerimaan (klik enter untuk cari)"
+            outlined
+            valid
+            class="col-4"
+            debounce="800"
+            :loading="store.loadingDataMauRet"
+            @keyup.enter="getData()"
+          />
+        </div>
+        <div class="row bg-dark text-white q-pa-xs q-mb-sm">
           <div
             class="col-auto"
             style="width:5%"
@@ -300,6 +304,7 @@
                 outlined
                 label="Jumlah Dikembalikan"
                 @update:model-value="updateJum($event,item,'jumlah_retur')"
+                @keyup.enter="simpan(i, item)"
               />
             </div>
             <div
@@ -311,7 +316,7 @@
                 icon="icon-mat-save"
                 color="primary"
                 round
-                :loading="store.loading"
+                :loading="store.loading && item.loading"
                 @click="simpan(i, item)"
               >
                 <q-tooltip
@@ -331,19 +336,99 @@
       <div class="bg-grey q-pa-sm text-white f-14 q-mb-md">
         Daftar Barang Dikembalikan
       </div>
-      <div><app-no-data-small /></div>
+      <div v-if="!store?.dataReturs?.length">
+        <app-no-data-small />
+      </div>
+      <div v-if="store?.dataReturs?.length">
+        <div class="row bg-dark text-white q-pa-xs q-mb-sm">
+          <div
+            class="col-auto"
+            style="width:5%"
+          >
+            No
+          </div>
+          <div
+            class="col-auto rinci"
+          >
+            Nama Obat
+          </div>
+          <div
+            class="col-auto rinci"
+          >
+            Kondisi Barang
+          </div>
+          <div
+            class="col-auto rinci"
+          >
+            Tanggal Rusak
+          </div>
+          <div
+            class="col-auto rinci"
+          >
+            Jumlah Retur
+          </div>
+          <div
+            class="col-auto rinci"
+          >
+            Nili Retur
+          </div>
+        </div>
+        <div
+          v-for="(rinc,j) in store?.dataReturs"
+          :key="j"
+        >
+          <div class="row q-col-gutter-sm q-mb-sm">
+            <div
+              class="col-auto"
+              style="width:5%"
+            >
+              {{ j+1 }}
+            </div>
+            <div
+              class="col-auto rinci"
+            >
+              {{ rinc?.mobatnew?.nama_obat }}
+            </div>
+            <div
+              class="col-auto rinci"
+            >
+              {{ rinc?.kondisi_barang }}
+            </div>
+            <div
+              class="col-auto rinci"
+            >
+              {{ humanDate(rinc?.tgl_rusak) }}
+            </div>
+            <div
+              class="col-auto rinci"
+            >
+              {{ rinc?.jumlah_retur }}
+            </div>
+            <div
+              class="col-auto rinci"
+            >
+              {{ formatRpDouble(rinc?.subtotal,2) }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script setup>
-import { humanDate } from 'src/modules/formatter'
+import { humanDate, formatRpDouble } from 'src/modules/formatter'
 import { date } from 'quasar'
 import { useReturPenyediaStore } from 'src/stores/simrs/farmasi/gudang/retur'
 import { ref } from 'vue'
 import { notifErrVue } from 'src/modules/utils'
 
 const store = useReturPenyediaStore()
-
+const refCariNoPenerimaan = ref(null)
+function getData() {
+  store.getDataMauRet().then(() => {
+    refCariNoPenerimaan.value.$refs.refInput.focus()
+  })
+}
 function setTanggal(val) {
   store.setForm('tgl_retur', date.formatDate(val, 'YYYY-MM-DD'))
 }
@@ -428,17 +513,23 @@ function validasi(index, dibayar) {
 }
 function simpan(index, item) {
   const dibayar = !!item.tgl_pembayaran
-  console.log(validasi(index, dibayar))
   if (validasi(index, dibayar)) {
     if (!item?.kondisi_barang || item?.kondisi_barang === '') return notifErrVue('Kondisi barang belum di isi')
     store.setForm('item', item)
-    store.simpanRetur()
+    store.simpanRetur(item)
+    // console.log('form', store.form)
+  } else {
+    notifErrVue('Cek kembali input anda')
   }
 }
 </script>
 <style lang="scss" scoped>
 .anak{
   width:calc(95% / 9);
+  overflow-wrap: anywhere;
+}
+.rinci{
+  width:calc(95% / 5);
   overflow-wrap: anywhere;
 }
 </style>
