bb7169638929681eaa7c0ff23b17a35213d71712
duro06 <andi.meka.meka.025@gmail.com>
2024-04-26 20:03:26 +0700

    tampil form barang rusak

--- src/pages/simrs/logistik/gudang/farmasi/barangrusak/comp/FormPage.vue
+++ src/pages/simrs/logistik/gudang/farmasi/barangrusak/comp/FormPage.vue
@@ -1,3 +1,234 @@
 <template>
-  Halaman from barang rusak
+  <div class="q-pa-sm">
+    <!-- header -->
+    <div>
+      <div class="row q-col-gutter-xs">
+        <!-- Obat -->
+        <div class="col-6">
+          <div class="row  items-center q-mb-sm">
+            <div class="col-3">
+              Obat
+            </div>
+
+            <div class="col-9">
+              <div class="row">
+                <div class="col-12">
+                  <app-autocomplete-new
+                    ref="refObat"
+                    v-model="store.form.kd_obat"
+                    label="Pilih Obat"
+                    autocomplete="nama_obat"
+                    option-label="nama_obat"
+                    option-value="kd_obat"
+                    outlined
+                    valid
+                    :loading="store.loadingObat"
+                    :source="store.obats"
+                    @on-select="store.obatSelected"
+                    @buang="store.setParams('q',$event)"
+                    @keyup.enter.stop="store.cariObat()"
+                  />
+                </div>
+              </div>
+              <div class="row">
+                <div
+                  v-if="!!store.form.kd_obat"
+                  class="f-10 text-weight-bold q-mr-sm"
+                >
+                  {{ store.form.kd_obat }}
+                </div>
+                <div
+                  v-if="!!store.form.tstok"
+                  class="text-italic f-10 text-weight-bold q-mr-xs"
+                >
+                  {{ store.form.tstok }}
+                </div>
+                <div
+                  v-if="!!store.form.satuan_kcl"
+                  class="text-italic f-10 q-mr-xs"
+                >
+                  {{ store.form.satuan_kcl }}
+                </div>
+                <div
+                  v-if="!!store.form.stok"
+                  class="text-italic f-10 text-weight-bold q-mr-xs"
+                >
+                  (total)
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- bacth -->
+        <div class="col-6">
+          <div class="row  items-center q-mb-sm">
+            <div class="col-3">
+              Nomor Batch
+            </div>
+
+            <div class="col-9">
+              <app-autocomplete-new
+                ref="refBatch"
+                v-model="store.form.no_batch"
+                label="Pilih Batch"
+                autocomplete="nobatch"
+                option-label="nobatch"
+                option-value="nobatch"
+                outlined
+                valid
+                :loading="store.loadingBatch"
+                :source="store.batchs"
+                @on-select="store.batchSelected"
+                @buang="store.setParams('batch',$event)"
+                @keyup.enter.stop="store.cariNoBatch()"
+              />
+            </div>
+          </div>
+        </div>
+        <!-- penerimaan -->
+        <div class="col-6">
+          <div class="row  items-center q-mb-sm">
+            <div class="col-3">
+              Nomor Penerimaan
+            </div>
+
+            <div class="col-9">
+              <app-autocomplete-new
+                ref="refPenerimaan"
+                v-model="store.form.nopenerimaan"
+                label="Pilih Penerimaan"
+                autocomplete="nopenerimaan"
+                :option-label="['nopenerimaan','pbf']"
+                option-value="nopenerimaan"
+                outlined
+                valid
+                :loading="store.loadingPenerimaan"
+                :source="store.penerimaans"
+                @on-select="store.penerimaanSelected"
+                @buang="store.setParams('noper',$event)"
+                @keyup.enter.stop="store.cariPenerimaan()"
+              />
+              <div
+                v-if="Object.keys(store.penerimaan).length"
+                class="row"
+              >
+                <div
+                  class="f-10 q-mr-sm"
+                >
+                  PBF :
+                </div>
+                <div
+                  class="f-10 text-weight-bold q-mr-sm"
+                >
+                  {{ store.penerimaan?.penerimaan?.pihakketiga.nama ?? 'PBF Tidak ditemukan' }}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- stok -->
+        <div class="col-6">
+          <div class="row  items-center q-mb-sm">
+            <div class="col-3">
+              Stok
+            </div>
+
+            <div class="col-9">
+              <app-input
+                v-model="store.form.stok"
+                label="Jumlah Stok"
+                outlined
+                valid
+                readonly
+              />
+            </div>
+          </div>
+        </div>
+        <!-- Status -->
+        <div class="col-6">
+          <div class="row  items-center q-mb-sm">
+            <div class="col-3">
+              Status
+            </div>
+
+            <div class="col-9">
+              <app-autocomplete
+                ref="refStatus"
+                v-model="store.form.status"
+                label="Pilih status"
+                outlined
+                valid
+                :source="store.statuses"
+              />
+            </div>
+          </div>
+        </div>
+        <!-- Jumlah -->
+        <div class="col-6">
+          <div class="row  items-center q-mb-sm">
+            <div class="col-3">
+              Jumlah Rusak
+            </div>
+
+            <div class="col-9">
+              <app-input
+                ref="refJumlah"
+                v-model="store.form.jumlah"
+                label="Jumlah Rusak"
+                outlined
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row q-my-lg justify-end">
+        <app-btn
+          label="simpan"
+          @click="simpan()"
+        />
+      </div>
+    </div>
+    <!-- hasil Input -->
+    <div>{{ store.form }}</div>
+  </div>
 </template>
+<script setup>
+import { useFormBarangRusakStore } from 'src/stores/simrs/farmasi/barangrusak/form'
+import { ref } from 'vue'
+
+const store = useFormBarangRusakStore()
+// auto
+const refObat = ref(null)
+const refBatch = ref(null)
+const refPenerimaan = ref(null)
+const refStatus = ref(null)
+// inp
+const refJumlah = ref(null)
+// eslint-disable-next-line no-unused-vars
+function resetvalidasi() {
+  refObat.value?.$refs?.refAuto?.resetValidation()
+  refBatch.value?.$refs?.refAuto?.resetValidation()
+  refPenerimaan.value?.$refs?.refAuto?.resetValidation()
+  refStatus.value?.$refs?.refAuto?.resetValidation()
+  refJumlah.value?.$refs?.refInput?.resetValidation()
+}
+function validasi() {
+  const obat = refObat.value?.$refs?.refAuto?.validate()
+  const batch = refBatch.value?.$refs?.refAuto?.validate()
+  const penerimaan = refPenerimaan.value?.$refs?.refAuto?.validate()
+  const status = refStatus.value?.$refs?.refAuto?.validate()
+  const jumlah = refJumlah.value?.$refs?.refInput?.validate()
+  if (
+    obat &&
+   batch &&
+   penerimaan &&
+   status &&
+   jumlah
+  ) return true
+  else return false
+}
+function simpan() {
+  validasi()
+  console.log('simpan', refObat.value?.$refs?.refAuto)
+}
+</script>
--- src/stores/simrs/farmasi/barangrusak/form.js
+++ src/stores/simrs/farmasi/barangrusak/form.js
@@ -1,12 +1,22 @@
 import { defineStore } from 'pinia'
+import { api } from 'src/boot/axios'
 
 export const useFormBarangRusakStore = defineStore('form_barang_rusak', {
   state: () => ({
     loading: false,
+    loadingObat: false,
+    loadingBatch: false,
+    loadingPenerimaan: false,
     params: {
       per_page: 10
     },
-    form: {}
+    form: {
+    },
+    obats: [],
+    batchs: [],
+    penerimaans: [],
+    statuses: ['Rusak', 'Kadalwarsa'],
+    penerimaan: {}
   }),
   actions: {
     setParams(key, val) {
@@ -15,9 +25,96 @@ export const useFormBarangRusakStore = defineStore('form_barang_rusak', {
     setForm(key, val) {
       this.form[key] = val
     },
+    obatSelected(val) {
+      this.setParams('kdobat', val)
+      this.setParams('nobatch', null)
+      this.form = {}
+      this.penerimaan = {}
+      const obat = this.obats.find(a => a.kd_obat === val)
+      if (obat) {
+        this.setForm('satuan_kcl', obat?.satuan_k)
+        this.setForm('satuan_bsr', obat?.satuan_b)
+        this.setForm('tstok', obat?.jumlah)
+      }
+      this.cariNoBatch()
+    },
+    batchSelected(val) {
+      this.setParams('nobatch', val)
+      this.cariPenerimaan()
+    },
+    penerimaanSelected(val) {
+      console.log('penerimaan terpilih', val)
+      this.setParams('penerimaan', val)
+      this.setForm('nopenerimaan', val)
+      delete this.form.isi
+      delete this.form.stok
+      this.penerimaan = {}
+      if (val === null) {
+        this.penerimaan = {}
+        return
+      }
+      const trm = this.penerimaans.find(t => t.nopenerimaan === val)
+      if (trm) {
+        this.penerimaan = trm
+        this.setForm('isi', trm?.penerimaan?.penerimaanrinci[0]?.isi ?? 0)
+        this.setForm('stok', parseFloat(trm.total))
+      }
+      // this.cariPenerimaan()
+    },
 
     getInitialData() {
-      // this.getPerusahan()
+      this.cariObat()
+    },
+    async cariObat() {
+      this.obats = []
+      const param = { params: this.params }
+      this.loadingObat = true
+      await api.get('v1/simrs/penunjang/farmasinew/barangrusak/obat', param)
+        .then(resp => {
+          this.loadingObat = false
+          this.obats = resp?.data
+        })
+        .catch(() => {
+          this.loadingObat = false
+        })
+    },
+    async cariNoBatch() {
+      this.batchs = []
+      const param = { params: this.params }
+      this.loadingBatch = true
+      await api.get('v1/simrs/penunjang/farmasinew/barangrusak/bacth', param)
+        .then(resp => {
+          this.loadingBatch = false
+          this.batchs = resp?.data
+        })
+        .catch(() => {
+          this.loadingBatch = false
+        })
+    },
+    async cariPenerimaan() {
+      this.penerimaans = []
+      const param = { params: this.params }
+      this.loadingPenerimaan = true
+      await api.get('v1/simrs/penunjang/farmasinew/barangrusak/penerimaan', param)
+        .then(resp => {
+          this.loadingPenerimaan = false
+          this.penerimaans = resp?.data
+          if (this.penerimaans?.length) {
+            this.penerimaans.forEach(trm => {
+              trm.pbf = trm?.penerimaan?.pihakketiga.nama ?? 'PBF Tidak ditemukan'
+            })
+          }
+          if (this.penerimaans?.length === 1) {
+            this.setForm('isi', this.penerimaans[0]?.penerimaan?.penerimaanrinci[0]?.isi ?? 0)
+            this.setForm('nopenerimaan', this.penerimaans[0]?.nopenerimaan)
+            this.setForm('stok', parseFloat(this.penerimaans[0]?.total))
+            this.setParams('penerimaan', this.penerimaans[0]?.nopenerimaan)
+            this.penerimaan = this.penerimaans[0]
+          }
+        })
+        .catch(() => {
+          this.loadingPenerimaan = false
+        })
     }
   }
 })
