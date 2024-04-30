c5f6770c29b1298834898567d939c1189de1a78e
duro06 <andi.meka.meka.025@gmail.com>
2024-04-30 21:19:54 +0700

    ada yang lupa

--- src/stores/simrs/farmasi/permintaanruangan/permintaan.js
+++ src/stores/simrs/farmasi/permintaanruangan/permintaan.js
@@ -196,7 +196,7 @@ export const useFarmasiPermintaanRuanganStore = defineStore('farmasi_permintaan_
             if (resp.data.rinci) {
               const rinc = resp.data.rinci
               if (rinc.kdobat) {
-                const anu = this.obats.filter(a => a.kd_obat === rinc.kdobat)
+                const anu = this.obats.filter(a => a.kdobat === rinc.kdobat)
                 if (anu.length) {
                   const obat = anu[0]
                   rinc.nama_obat = obat.nama_obat
