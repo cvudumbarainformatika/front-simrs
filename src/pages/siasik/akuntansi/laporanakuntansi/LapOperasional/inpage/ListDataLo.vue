ec23eea9ecb00160304cd71e646f659eca6497c9
pigartpigaro <vichoirul@gmail.com>
2024-11-03 22:16:29 +0700

    rev

--- src/pages/siasik/akuntansi/laporanakuntansi/LapOperasional/inpage/ListDataLo.vue
+++ src/pages/siasik/akuntansi/laporanakuntansi/LapOperasional/inpage/ListDataLo.vue
@@ -11,13 +11,15 @@
             <thead>
               <tr class="bg-dark text-white max-width">
                 <th>KODE REKENING</th>
-                <th style="width: 40%;">
+                <th style="width: 70%;">
                   URAIAN
                 </th>
-                <th>NILAI (Rp.)</th>
+                <th style="width: 20%;">
+                  NILAI (Rp.)
+                </th>
               </tr>
             </thead>
-            <tbody>
+            <tbody v-if="store.reqs.levelberapa === 6">
               <tr
                 v-for="it in store.hasilpendapatan" :key="it"
                 :class="it.kode.length <= 12 ? 'text-bold' : '' "
@@ -43,6 +45,222 @@
               <tr
                 v-for="it in store.hasilbeban" :key="it"
                 :class="it.kode.length <= 12 ? 'text-bold' : '' "
+              >
+                <td>
+                  {{ it.kode }}
+                </td>
+                <td>
+                  {{ it.uraian }}
+                </td>
+                <td class="text-right">
+                  {{ formattanpaRp(it.nilai) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  JUMLAH BEBAN DAERAH
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(totalBeban()) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  SURPLUS / DEFISIT LO
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(surplusdefisit()) }}
+                </td>
+              </tr>
+            </tbody>
+            <tbody v-if="store.reqs.levelberapa === 5">
+              <tr
+                v-for="it in store.hasilpendapatan" :key="it"
+                :class="it.kode.length <= 9 ? 'text-bold' : '' "
+              >
+                <td>
+                  {{ it.kode }}
+                </td>
+                <td>
+                  {{ it.uraian }}
+                </td>
+                <td class="text-right">
+                  {{ formattanpaRp(it.nilai) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  JUMLAH PENDAPATAN DAERAH-LO
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(totalPendapatan()) }}
+                </td>
+              </tr>
+              <tr
+                v-for="it in store.hasilbeban" :key="it"
+                :class="it.kode.length <= 9 ? 'text-bold' : '' "
+              >
+                <td>
+                  {{ it.kode }}
+                </td>
+                <td>
+                  {{ it.uraian }}
+                </td>
+                <td class="text-right">
+                  {{ formattanpaRp(it.nilai) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  JUMLAH BEBAN DAERAH
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(totalBeban()) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  SURPLUS / DEFISIT LO
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(surplusdefisit()) }}
+                </td>
+              </tr>
+            </tbody>
+            <tbody v-if="store.reqs.levelberapa === 4">
+              <tr
+                v-for="it in store.hasilpendapatan" :key="it"
+                :class="it.kode.length <= 6 ? 'text-bold' : '' "
+              >
+                <td>
+                  {{ it.kode }}
+                </td>
+                <td>
+                  {{ it.uraian }}
+                </td>
+                <td class="text-right">
+                  {{ formattanpaRp(it.nilai) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  JUMLAH PENDAPATAN DAERAH-LO
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(totalPendapatan()) }}
+                </td>
+              </tr>
+              <tr
+                v-for="it in store.hasilbeban" :key="it"
+                :class="it.kode.length <= 6 ? 'text-bold' : '' "
+              >
+                <td>
+                  {{ it.kode }}
+                </td>
+                <td>
+                  {{ it.uraian }}
+                </td>
+                <td class="text-right">
+                  {{ formattanpaRp(it.nilai) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  JUMLAH BEBAN DAERAH
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(totalBeban()) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  SURPLUS / DEFISIT LO
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(surplusdefisit()) }}
+                </td>
+              </tr>
+            </tbody>
+            <tbody v-if="store.reqs.levelberapa === 3">
+              <tr
+                v-for="it in store.hasilpendapatan" :key="it"
+                :class="it.kode.length <= 3 ? 'text-bold' : '' "
+              >
+                <td>
+                  {{ it.kode }}
+                </td>
+                <td>
+                  {{ it.uraian }}
+                </td>
+                <td class="text-right">
+                  {{ formattanpaRp(it.nilai) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  JUMLAH PENDAPATAN DAERAH-LO
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(totalPendapatan()) }}
+                </td>
+              </tr>
+              <tr
+                v-for="it in store.hasilbeban" :key="it"
+                :class="it.kode.length <= 3 ? 'text-bold' : '' "
+              >
+                <td>
+                  {{ it.kode }}
+                </td>
+                <td>
+                  {{ it.uraian }}
+                </td>
+                <td class="text-right">
+                  {{ formattanpaRp(it.nilai) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  JUMLAH BEBAN DAERAH
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(totalBeban()) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  SURPLUS / DEFISIT LO
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(surplusdefisit()) }}
+                </td>
+              </tr>
+            </tbody>
+            <tbody v-if="store.reqs.levelberapa === 2 || store.reqs.levelberapa === 1">
+              <tr
+                v-for="it in store.hasilpendapatan" :key="it"
+                :class="it.kode.length <= 1 ? 'text-bold' : '' "
+              >
+                <td>
+                  {{ it.kode }}
+                </td>
+                <td>
+                  {{ it.uraian }}
+                </td>
+                <td class="text-right">
+                  {{ formattanpaRp(it.nilai) }}
+                </td>
+              </tr>
+              <tr class="bg-grey-2">
+                <td class="text-right text-bold" colspan="2">
+                  JUMLAH PENDAPATAN DAERAH-LO
+                </td>
+                <td class="text-right text-bold">
+                  {{ formattanpaRp(totalPendapatan()) }}
+                </td>
+              </tr>
+              <tr
+                v-for="it in store.hasilbeban" :key="it"
+                :class="it.kode.length <= 1 ? 'text-bold' : '' "
               >
                 <td>
                   {{ it.kode }}
