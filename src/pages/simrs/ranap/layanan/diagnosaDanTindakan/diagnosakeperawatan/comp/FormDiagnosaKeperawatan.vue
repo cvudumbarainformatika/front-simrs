be37bbced155bb6dee7273133b87b9fac376b73a
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-11-12 01:04:35 +0700

    fix: plann diagnosa keperawatan done

--- src/pages/simrs/ranap/layanan/diagnosaDanTindakan/diagnosakeperawatan/comp/FormDiagnosaKeperawatan.vue
+++ src/pages/simrs/ranap/layanan/diagnosaDanTindakan/diagnosakeperawatan/comp/FormDiagnosaKeperawatan.vue
@@ -166,6 +166,38 @@
                 size="sm"
               />
             </div>
+
+            <div
+              v-if="item.intervensis?.filter(x=>x.group === 'plann').length"
+              class="q-mb-sm"
+            >
+              <q-separator class="q-my-xs" />
+              <div class="">
+                <q-chip
+                  outline
+                  color="pink"
+                  text-color="white"
+                  dense
+                  size="sm"
+                >
+                  Kolaborasi
+                </q-chip>
+              </div>
+              <q-option-group
+                v-model="store.selectIntervensis"
+                :options="item.intervensis?.filter(x=>x.group === 'plann').map(x=>{
+                  return {
+                    label:x?.nama,
+                    value:x?.id + '||' + item.kode
+                  }
+                })"
+                color="primary"
+                inline
+                dense
+                type="checkbox"
+                size="sm"
+              />
+            </div>
           </div>
 
           <div style="margin-bottom:100px" />
