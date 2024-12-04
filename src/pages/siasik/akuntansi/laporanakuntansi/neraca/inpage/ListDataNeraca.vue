73d178b615504ab3470f047346165d73c2b7979d
pigartpigaro <vichoirul@gmail.com>
2024-12-05 00:57:21 +0700

    neraca

---
+++ src/pages/siasik/akuntansi/laporanakuntansi/neraca/inpage/ListDataNeraca.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    coba
+  </div>
+</template>
+<script setup>
+// import { useNeracaStore } from 'src/stores/siasik/laporan/neraca/neraca'
+// import { onMounted } from 'vue'
+
+// const store = useNeracaStore()
+// onMounted(() => {
+//   store.getDataNeraca()
+// })
+
+</script>
