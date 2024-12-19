9811a38ef3a9b9940b28e216a0e677d1f8e82c60
cvudumbarainformatika <cvudumbarainformatika@gmail.com>
2024-12-19 08:47:11 +0700

    rev: summary, history-anestesi

---
+++ src/pages/simrs/ranap/layanan/prabedah/praanestesi/comp/DialogRajal.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-dialog position="right">
+    <q-card style="width: 500px; height: 100vh">
+      <q-card-section class="column full-height q-pa-none">
+        <div class="col-auto q-pa-md">
+          <div class="text-weight-bold">
+            History Pra Anastesi (Rajal)
+          </div>
+          <div class="text-grey">
+            Diambil dari Poli Anestesi Kunjungan Terakhir
+          </div>
+        </div>
+        <div class="col full-height scroll bg-grey-4 q-pa-lg">
+          {{ data }}
+        </div>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+const props = defineProps({
+  pasien: {
+    type: Object,
+    default: null
+  },
+  data: {
+    type: Object,
+    default: null
+  }
+})
+
+onMounted(() => {
+  console.log(props.pasien)
+})
+
+</script>
